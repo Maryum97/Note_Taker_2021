b970e91eba09891eae09a04d70fc3bc02aa97c58
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-04 15:16:37 +0000

    corrected path routes

--- apiRoutes.js
+++ apiRoutes.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 module.exports = (app) => {
     // GET `/notes` - Should return the `notes.html` file.
     app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+        res.sendFile(path.join(__dirname, "./main-files/notes.html"));
     });
 
     // GET `/` - Should return the `index.html` file
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "./main-files/index.html"));
     });
 }
